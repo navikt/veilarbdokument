32d31568feadfbd9e7a89435f5c08611d3e03c6f
matiasvinjevoll <matias.vinjevoll@nav.no>
2019-05-20 14:28:38 +0200

    KAIZEN-0 Håndter at veilarbabac url ikke settes I det tilfellet brukes cluster url

--- src/main/java/no/nav/fo/veilarb/dokument/ApplicationConfig.java
+++ src/main/java/no/nav/fo/veilarb/dokument/ApplicationConfig.java
@@ -97,14 +97,22 @@ public class ApplicationConfig implements ApiApplication {
     @Bean
     public VeilarbAbacPepClient pepClient(Pep pep) {
 
-        return VeilarbAbacPepClient.ny()
+        String overrideUrl = getProperty(VEILARBABAC_API_URL_PROPERTY);
+
+        VeilarbAbacPepClient.Builder builder = VeilarbAbacPepClient.ny()
                 .medPep(pep)
                 .medSystemUserTokenProvider(() -> systemUserTokenProvider().getToken())
                 .brukAktoerId(() -> true)
                 .sammenlikneTilgang(() -> false)
-                .foretrekkVeilarbAbacResultat(() -> true)
-                .medVeilarbAbacUrl(getProperty(VEILARBABAC_API_URL_PROPERTY))
-                .bygg();
+                .foretrekkVeilarbAbacResultat(() -> true);
+
+        if (overrideUrl != null) {
+            return builder
+                    .medVeilarbAbacUrl(overrideUrl)
+                    .bygg();
+        } else {
+            return builder.bygg();
+        }
     }
 
     public static String getDokumentproduksjonEndpointUrl() {
