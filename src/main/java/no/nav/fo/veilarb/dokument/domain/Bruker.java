8f76c0d95f9f9c18c3db0586a10b066c97843ae3
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-02-14 10:04:43 +0100

    FO-2599 bruk PepClient

---
+++ src/main/java/no/nav/fo/veilarb/dokument/domain/Bruker.java
@@ -0,0 +1,9 @@
+package no.nav.fo.veilarb.dokument.domain;
+
+import lombok.Value;
+
+@Value
+public class Bruker {
+    String fnr;
+    String aktorId;
+}
--- src/test/java/no/nav/fo/veilarb/dokument/service/OppfolgingssakServiceTest.java
+++ src/test/java/no/nav/fo/veilarb/dokument/service/OppfolgingssakServiceTest.java
@@ -1,7 +1,7 @@
 package no.nav.fo.veilarb.dokument.service;
 
-import no.nav.apiapp.security.veilarbabac.Bruker;
 import no.nav.fo.veilarb.dokument.domain.ArenaOppfolgingssak;
+import no.nav.fo.veilarb.dokument.domain.Bruker;
 import no.nav.fo.veilarb.dokument.domain.Sak;
 import org.junit.Test;
 
@@ -15,7 +15,7 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class OppfolgingssakServiceTest {
 
-    private Bruker bruker = Bruker.fraFnr("fnr").medAktoerId("aktorId");
+    private Bruker bruker = new Bruker("fnr", "aktorId");
     private SakService sakService = mock(SakService.class);
     private ArenaService arenaService = mock(ArenaService.class);
     private OppfolgingssakService oppfolgingssakService = new OppfolgingssakService(sakService, arenaService);
@@ -24,11 +24,11 @@ public class OppfolgingssakServiceTest {
     public void hentOppfolgingssak__henter_sak_fra_gsak_dersom_en_oppfolgingssak() {
         Sak forventetSak = new Sak(0, null, null, null, null);
 
-        when(sakService.finnOppfolgingssaker(bruker.getAktoerId())).thenReturn(Collections.singletonList(forventetSak));
+        when(sakService.finnOppfolgingssaker(bruker.getAktorId())).thenReturn(Collections.singletonList(forventetSak));
 
         assertThat(oppfolgingssakService.hentOppfolgingssak(bruker)).isEqualTo(forventetSak);
 
-        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktoerId());
+        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktorId());
         verify(arenaService, times(0)).oppfolgingssak(any());
         verify(sakService, times(0)).opprettOppfolgingssak(any(), any());
     }
@@ -37,28 +37,28 @@ public class OppfolgingssakServiceTest {
     public void hentOppfolgingssak__henter_sak_fra_arena_og_oppretter_i_gsak_dersom_ingen_oppfolgingssak_i_gsak() {
         Sak forventetSak = new Sak(0, null, null, "fagsakNr", null);
 
-        when(sakService.finnOppfolgingssaker(bruker.getAktoerId())).thenReturn(Collections.emptyList());
-        when(arenaService.oppfolgingssak(bruker.getFoedselsnummer())).thenReturn(
+        when(sakService.finnOppfolgingssaker(bruker.getAktorId())).thenReturn(Collections.emptyList());
+        when(arenaService.oppfolgingssak(bruker.getFnr())).thenReturn(
                 Optional.of(new ArenaOppfolgingssak(forventetSak.fagsakNr()))
         );
-        when(sakService.opprettOppfolgingssak(bruker.getAktoerId(), forventetSak.fagsakNr())).thenReturn(forventetSak);
+        when(sakService.opprettOppfolgingssak(bruker.getAktorId(), forventetSak.fagsakNr())).thenReturn(forventetSak);
 
         assertThat(oppfolgingssakService.hentOppfolgingssak(bruker)).isEqualTo(forventetSak);
 
-        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktoerId());
-        verify(arenaService, times(1)).oppfolgingssak(bruker.getFoedselsnummer());
-        verify(sakService, times(1)).opprettOppfolgingssak(bruker.getAktoerId(), forventetSak.fagsakNr());
+        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktorId());
+        verify(arenaService, times(1)).oppfolgingssak(bruker.getFnr());
+        verify(sakService, times(1)).opprettOppfolgingssak(bruker.getAktorId(), forventetSak.fagsakNr());
     }
 
     @Test(expected = IllegalStateException.class)
     public void hentOppfolgingssak__feiler_dersom_ingen_oppfolgingssak_verken_i_arena_eller_gsak() {
-        when(sakService.finnOppfolgingssaker(bruker.getAktoerId())).thenReturn(Collections.emptyList());
-        when(arenaService.oppfolgingssak(bruker.getFoedselsnummer())).thenReturn(Optional.empty());
+        when(sakService.finnOppfolgingssaker(bruker.getAktorId())).thenReturn(Collections.emptyList());
+        when(arenaService.oppfolgingssak(bruker.getFnr())).thenReturn(Optional.empty());
 
         oppfolgingssakService.hentOppfolgingssak(bruker);
 
-        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktoerId());
-        verify(arenaService, times(1)).oppfolgingssak(bruker.getFoedselsnummer());
+        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktorId());
+        verify(arenaService, times(1)).oppfolgingssak(bruker.getFnr());
         verify(sakService, times(0)).opprettOppfolgingssak(any(), any());
     }
 
@@ -67,15 +67,15 @@ public class OppfolgingssakServiceTest {
         Sak forventetSak = new Sak(0, null, null, "forventetFagsakNr", null);
         Sak uforventetSak = new Sak(0, null, null, "uforventetFagsakNr", null);
 
-        when(sakService.finnOppfolgingssaker(bruker.getAktoerId())).thenReturn(Arrays.asList(forventetSak, uforventetSak));
-        when(arenaService.oppfolgingssak(bruker.getFoedselsnummer())).thenReturn(
+        when(sakService.finnOppfolgingssaker(bruker.getAktorId())).thenReturn(Arrays.asList(forventetSak, uforventetSak));
+        when(arenaService.oppfolgingssak(bruker.getFnr())).thenReturn(
                 Optional.of(new ArenaOppfolgingssak(forventetSak.fagsakNr()))
         );
 
         assertThat(oppfolgingssakService.hentOppfolgingssak(bruker)).isEqualTo(forventetSak);
 
-        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktoerId());
-        verify(arenaService, times(1)).oppfolgingssak(bruker.getFoedselsnummer());
+        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktorId());
+        verify(arenaService, times(1)).oppfolgingssak(bruker.getFnr());
         verify(sakService, times(0)).opprettOppfolgingssak(any(), any());
     }
 
@@ -84,13 +84,13 @@ public class OppfolgingssakServiceTest {
         Sak sak1 = new Sak(0, null, null, null, null);
         Sak sak2 = new Sak(0, null, null, null, null);
 
-        when(sakService.finnOppfolgingssaker(bruker.getAktoerId())).thenReturn(Arrays.asList(sak1, sak2));
-        when(arenaService.oppfolgingssak(bruker.getFoedselsnummer())).thenReturn(Optional.empty());
+        when(sakService.finnOppfolgingssaker(bruker.getAktorId())).thenReturn(Arrays.asList(sak1, sak2));
+        when(arenaService.oppfolgingssak(bruker.getFnr())).thenReturn(Optional.empty());
 
         oppfolgingssakService.hentOppfolgingssak(bruker);
 
-        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktoerId());
-        verify(arenaService, times(1)).oppfolgingssak(bruker.getFoedselsnummer());
+        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktorId());
+        verify(arenaService, times(1)).oppfolgingssak(bruker.getFnr());
         verify(sakService, times(0)).opprettOppfolgingssak(any(), any());
     }
 
@@ -100,16 +100,16 @@ public class OppfolgingssakServiceTest {
         Sak uforventetSak1 = new Sak(0, null, null, "uforventetFagsakNr1", null);
         Sak uforventetSak2 = new Sak(0, null, null, "uforventetFagsakNr2", null);
 
-        when(sakService.finnOppfolgingssaker(bruker.getAktoerId())).thenReturn(Arrays.asList(uforventetSak1, uforventetSak2));
-        when(arenaService.oppfolgingssak(bruker.getFoedselsnummer())).thenReturn(
+        when(sakService.finnOppfolgingssaker(bruker.getAktorId())).thenReturn(Arrays.asList(uforventetSak1, uforventetSak2));
+        when(arenaService.oppfolgingssak(bruker.getFnr())).thenReturn(
                 Optional.of(new ArenaOppfolgingssak(forventetSak.fagsakNr()))
         );
-        when(sakService.opprettOppfolgingssak(bruker.getAktoerId(), forventetSak.fagsakNr())).thenReturn(forventetSak);
+        when(sakService.opprettOppfolgingssak(bruker.getAktorId(), forventetSak.fagsakNr())).thenReturn(forventetSak);
 
         assertThat(oppfolgingssakService.hentOppfolgingssak(bruker)).isEqualTo(forventetSak);
 
-        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktoerId());
-        verify(arenaService, times(1)).oppfolgingssak(bruker.getFoedselsnummer());
-        verify(sakService, times(1)).opprettOppfolgingssak(bruker.getAktoerId(), forventetSak.fagsakNr());
+        verify(sakService, times(1)).finnOppfolgingssaker(bruker.getAktorId());
+        verify(arenaService, times(1)).oppfolgingssak(bruker.getFnr());
+        verify(sakService, times(1)).opprettOppfolgingssak(bruker.getAktorId(), forventetSak.fagsakNr());
     }
 }
