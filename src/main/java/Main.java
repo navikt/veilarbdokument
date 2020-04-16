587aa8438e182ee66829a76e2fb4228d2f155179
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-04-16 08:53:22 +0200

    bump common

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import no.nav.apiapp.ApiApp;
-import no.nav.brukerdialog.tools.SecurityConstants;
 import no.nav.common.nais.utils.NaisUtils;
 import no.nav.fo.veilarb.dokument.config.ApplicationConfig;
 import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
@@ -20,10 +19,6 @@ public class Main {
         //CXF
         System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
         System.setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
-
-        //OIDC
-        System.setProperty(SecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
-        System.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
 
         ApiApp.runApp(ApplicationConfig.class, args);
     }
--- src/test/java/no/nav/fo/veilarb/dokument/TestConfig.java
+++ src/test/java/no/nav/fo/veilarb/dokument/TestConfig.java
@@ -1,18 +1,13 @@
 package no.nav.fo.veilarb.dokument;
 
-import no.nav.fasit.ServiceUser;
-import no.nav.fasit.WebServiceEndpoint;
-import no.nav.fasit.dto.RestService;
+import no.nav.common.nais.utils.NaisUtils;
+import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
+import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 import no.nav.testconfig.ApiAppTest;
 
-import static no.nav.brukerdialog.security.Constants.*;
-import static no.nav.fasit.FasitUtils.*;
-import static no.nav.fasit.FasitUtils.Zone.FSS;
 import static no.nav.fo.veilarb.dokument.config.ApplicationConfig.*;
 import static no.nav.fo.veilarb.dokument.utils.TestUtils.lagFssUrl;
 import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacServiceConfig.ABAC_ENDPOINT_URL_PROPERTY_NAME;
-import static no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants.SYSTEMUSER_PASSWORD;
-import static no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants.SYSTEMUSER_USERNAME;
 import static no.nav.sbl.featuretoggle.unleash.UnleashServiceConfig.UNLEASH_API_URL_PROPERTY_NAME;
 import static no.nav.sbl.util.EnvironmentUtils.*;
 import static no.nav.sbl.util.EnvironmentUtils.Type.PUBLIC;
@@ -22,50 +17,31 @@ public class TestConfig {
 
     private static final String APPLICATION_NAME = "veilarbdokument";
     private static final String SERVICE_USER_NAME = "srv" + APPLICATION_NAME;
+    private static final String TESTMILJO = "q1";
 
     public static void setupTestContext() {
         ApiAppTest.setupTestContext(ApiAppTest.Config.builder().applicationName(APPLICATION_NAME).build());
 
-        ServiceUser serviceUser = getServiceUser(SERVICE_USER_NAME, APPLICATION_NAME, FSS);
-        RestService veilarbLogin = getRestService("veilarblogin.redirect-url");
-        ServiceUser issoRpUser = getServiceUser("isso-rp-user", APPLICATION_NAME, FSS);
+        NaisUtils.Credentials serviceUser = null; // TODO
 
-        setProperty(SYSTEMUSER_USERNAME, serviceUser.getUsername(), PUBLIC);
-        setProperty(SYSTEMUSER_PASSWORD, serviceUser.getPassword(), SECRET);
-        setProperty(resolveSrvUserPropertyName(), serviceUser.getUsername(), PUBLIC);
-        setProperty(resolverSrvPasswordPropertyName(), serviceUser.getPassword(), SECRET);
-        setProperty(ISSO_HOST_URL_PROPERTY_NAME, getBaseUrl("isso-host"), PUBLIC);
-        setProperty(ISSO_RP_USER_USERNAME_PROPERTY_NAME, issoRpUser.username, PUBLIC);
-        setProperty(ISSO_RP_USER_PASSWORD_PROPERTY_NAME, issoRpUser.password, SECRET);
-        setProperty(ISSO_JWKS_URL_PROPERTY_NAME, getBaseUrl("isso-jwks"), PUBLIC);
-        setProperty(ISSO_ISSUER_URL_PROPERTY_NAME, getBaseUrl("isso-issuer"), PUBLIC);
-        setProperty(OIDC_REDIRECT_URL, veilarbLogin.getUrl(), PUBLIC);
-        setProperty(ISSO_ISALIVE_URL_PROPERTY_NAME, getBaseUrl("isso.isalive", FSS), PUBLIC);
+        setProperty(resolveSrvUserPropertyName(), serviceUser.username, PUBLIC);
+        setProperty(resolverSrvPasswordPropertyName(), serviceUser.password, SECRET);
+        setProperty(CredentialConstants.SYSTEMUSER_USERNAME, serviceUser.username, PUBLIC);
+        setProperty(CredentialConstants.SYSTEMUSER_PASSWORD, serviceUser.password, SECRET);
+        setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username, PUBLIC);
+        setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password, SECRET);
+
+        setProperty(OPENAM_DISCOVERY_URL, "https://isso-q.adeo.no/isso/oauth2/.well-known/openid-configuration", PUBLIC);
+        setProperty(VEILARBLOGIN_OPENAM_CLIENT_ID, "TODO", PUBLIC);
+        setProperty(VEILARBLOGIN_OPENAM_REFRESH_URL, "trengs ikke", PUBLIC);
+
         setProperty(UNLEASH_API_URL_PROPERTY_NAME, "https://unleash.nais.adeo.no/api/", PUBLIC);
-        setProperty(VEILARBABAC_API_URL_PROPERTY, lagFssUrl("veilarbabac", false), PUBLIC);
-        setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, "https://wasapp-" + getDefaultEnvironment() + ".adeo.no/asm-pdp/authorize", PUBLIC);
-        setProperty(VEILARBARENA_API_URL_PROPERTY, lagFssUrl("veilarbarena", false), PUBLIC);
-        setProperty(VEILARBVEILEDER_API_URL_PROPERTY, lagFssUrl("veilarbveileder", true) + "api/", PUBLIC);
-        setProperty(SAK_API_URL, lagFssUrl("sak", false), PUBLIC);
-        setProperty("VEILARBABAC", lagFssUrl("veilarbabac", false), PUBLIC);
-
-        WebServiceEndpoint dokumentproduksjonEndpoint = getWebServiceEndpoint("Dokumentproduksjon_v3", getDefaultEnvironment());
-        setProperty(
-                DOKUMENTPRODUKSJON_ENDPOINT_URL,
-                dokumentproduksjonEndpoint.getUrl(),
-                PUBLIC);
-
-        WebServiceEndpoint aktorEndpoint = getWebServiceEndpoint("Aktoer_v2", getDefaultEnvironment());
-        setProperty(
-                AKTOR_ENDPOINT_URL,
-                aktorEndpoint.getUrl(),
-                PUBLIC);
-
-        setProperty(
-                SECURITYTOKENSERVICE_URL,
-                getBaseUrl("securityTokenService", FSS),
-                PUBLIC
-        );
-
+        setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, "https://wasapp-" + TESTMILJO + ".adeo.no/asm-pdp/authorize", PUBLIC);
+        setProperty(VEILARBVEILEDER_API_URL_PROPERTY, lagFssUrl("veilarbveileder", TESTMILJO, true) + "api/", PUBLIC);
+        setProperty(VEILARBARENA_API_URL_PROPERTY, lagFssUrl("veilarbarena", TESTMILJO, true) + "api", PUBLIC);
+        setProperty(SAK_API_URL, lagFssUrl("sak", TESTMILJO, false), PUBLIC);
+        setProperty(DOKUMENTPRODUKSJON_ENDPOINT_URL, lagFssUrl("dokprod", TESTMILJO, true) + "ws/dokumentproduksjon/v3", PUBLIC);
+        setProperty(AKTOR_ENDPOINT_URL, "https://app-" + TESTMILJO + ".adeo.no/aktoerregister/ws/Aktoer/v2", PUBLIC);
+        setProperty(SECURITYTOKENSERVICE_URL, "https://sts-" + TESTMILJO + ".preprod.local/SecurityTokenServiceProvider/", PUBLIC);
     }
 }
