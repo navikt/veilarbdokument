163254e0db74761e58c18d7ddf3350a0de587246
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-06-11 13:32:12 +0200

    Håndter feil ved sending av metrikker

--- src/main/java/no/nav/fo/veilarb/dokument/service/MetrikkService.java
+++ src/main/java/no/nav/fo/veilarb/dokument/service/MetrikkService.java
@@ -1,15 +1,21 @@
 package no.nav.fo.veilarb.dokument.service;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.fo.veilarb.dokument.domain.MalType;
 import no.nav.metrics.MetricsFactory;
 
+@Slf4j
 public class MetrikkService {
 
-    private static final MeterRegistry meterRegistry = MetricsFactory.getMeterRegistry();;
+    private static final MeterRegistry meterRegistry = MetricsFactory.getMeterRegistry();
 
     public static void rapporterSak(String status) {
-        meterRegistry.counter("sak", "status", status).increment();
+        try {
+            meterRegistry.counter("sak", "status", status).increment();
+        } catch (Throwable t) {
+            log.error("Feil ved rapportering av metrikk", t);
+        }
     }
 
     public static void rapporterDokumentutkast(MalType mal) {
@@ -29,6 +35,10 @@ public class MetrikkService {
     }
 
     private static void rapporterDokumenthendelse(String hendelse, MalType mal) {
-        meterRegistry.counter(hendelse, "mal", mal.kode).increment();
+        try {
+            meterRegistry.counter(hendelse, "mal", mal.kode).increment();
+        } catch (Throwable t) {
+            log.error("Feil ved rapportering av metrikk", t);
+        }
     }
 }
