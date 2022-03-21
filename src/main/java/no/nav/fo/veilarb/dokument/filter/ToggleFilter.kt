f995b50a87eae09b1159dc5095727cf9db621c02
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-03-21 07:57:43 +0100

    Skru på v2 api

--- src/main/java/no/nav/fo/veilarb/dokument/filter/ToggleFilter.kt
+++ src/main/java/no/nav/fo/veilarb/dokument/filter/ToggleFilter.kt
@@ -3,7 +3,6 @@ package no.nav.fo.veilarb.dokument.filter
 import no.nav.common.featuretoggle.UnleashService
 import no.nav.common.utils.EnvironmentUtils
 import no.nav.fo.veilarb.dokument.util.Toggles.VEILARBDOKUMENT_ENABLED_TOGGLE
-import no.nav.fo.veilarb.dokument.util.Toggles.VEILARBDOKUMENT_V2_API_ENABLED_TOGGLE
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 
@@ -18,12 +17,6 @@ class ToggleFilter(private val unleashService: UnleashService) : Filter {
             httpServletRequest.servletPath.startsWith("/internal") ||
             unleashService.isEnabled(VEILARBDOKUMENT_ENABLED_TOGGLE)
         ) {
-
-            if (httpServletRequest.servletPath.startsWith("/api/v2") &&
-                    !(isDevelopment && unleashService.isEnabled(VEILARBDOKUMENT_V2_API_ENABLED_TOGGLE))) {
-                throw IllegalStateException("ikke tilgjengelig")
-            }
-
             chain.doFilter(request, response)
         } else {
             throw IllegalStateException("ikke tilgjengelig")
